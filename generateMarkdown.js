7c69b294c452efd03229ceea85718074afb5cffc
James Brooks <60955280+Jahugawugasuga@users.noreply.github.com>
2020-05-02 09:55:18 -0600

    updated functionality
    updated formatting and fixed bug where this file wasn't being run from main file.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,16 +1,28 @@
-function generateMarkdown(answers) {
+function generateMarkdown(answers,data) {
   return `
-  ![Github user](${response.avatar_url})
-# ${answers.title}
-###${answers.description}
-###${answers.installation}
-###${answers.usage}
-###${answers.technology}
-###${answers.licenses}
-###${answers.contributers}
-###${answers.tests}
-###${answers.questions}
-`;
+![github avatar](${data.avatar_url})\n
+![github badge](https://img.shields.io/badge/License-${data.license}-red)
+ ## Application
+ ${answers.title} 
+### Project Description
+${answers.description}
+### How to Install
+${answers.installation}
+### How to Use this Application
+${answers.usage}
+### Techology Used
+${answers.technology}
+### Licenses
+${answers.licenses}
+###
+${answers.contributers}
+### Testing
+${answers.tests}
+
+
+`
+
 }
 
 module.exports = generateMarkdown;
+
